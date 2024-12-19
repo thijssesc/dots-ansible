017eb36ab765d94f65ea455b7dd9c151e8943f88
Sjoerd Thijsse <sjoerdthijsse@gmail.com>
2024-12-19 16:26:54 +0100

    fix: fist set of fixes

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 
 sudo pacman -S --needed --noconfirm python-pipx
-pipx install ansible --force
+pipx install ansible ansible-core
 
 export PATH="$PATH:$HOME/.local/bin"
 
 ansible-galaxy install -r requirements.yml
-ansible-playbook main.yml -i "${1:-laptop}"
+ansible-playbook main.yml -i "${1:-laptop}" \
+ --tags system,dots,pacman,aur,dash,firefox,neovim,suckless
